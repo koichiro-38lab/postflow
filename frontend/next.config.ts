0de95c47a80c1e3b8342ca2e886bf2a9e62c8f9a
koichiro-38lab <koichiro.38lab@gmail.com>
2025-09-27 12:27:19 +0900

    frontend(config): Next.js画像設定の更新

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "host.docker.internal",
+				port: "9000",
+				pathname: "/**",
+			},
+			{
+				protocol: "http",
+				hostname: "localhost",
+				port: "9000",
+				pathname: "/**",
+			},
+			{
+				protocol: "http",
+				hostname: "100.118.151.11",
+				port: "9000",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
