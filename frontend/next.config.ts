fb69f009c9d047d5e627538bbdac442b1a8564eb
koichiro-38lab <koichiro.38lab@gmail.com>
2025-09-29 15:52:01 +0900

    frontend(config): 依存関係と設定ファイルの更新

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,28 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "host.docker.internal",
-				port: "9000",
-				pathname: "/**",
-			},
-			{
-				protocol: "http",
-				hostname: "localhost",
-				port: "9000",
-				pathname: "/**",
-			},
-			{
-				protocol: "http",
-				hostname: "100.118.151.11",
-				port: "9000",
-				pathname: "/**",
-			},
-		],
-	},
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "host.docker.internal",
+                port: "9000",
+                pathname: "/**",
+            },
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "9000",
+                pathname: "/**",
+            },
+            {
+                protocol: "http",
+                hostname: "100.118.151.11",
+                port: "9000",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
